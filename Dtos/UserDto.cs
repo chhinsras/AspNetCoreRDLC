b0d2f7f29ec3b2716ba09a343b4a44fef44393c9
Chhin Sras <chhinsras@Chhins-MacBook-Air.local>
2020-10-08 11:32:37 +0700

    AspNetCore3.1 with RDLC Report

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+namespace AspNetCoreRDLC.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
